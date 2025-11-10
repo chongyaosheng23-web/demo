accde9c4c116b946a2c74280e199da061ffb3655
YS <chongyaosheng23@gmail.com>
2025-11-11 02:07:35 +0800

    Done update for 10/11/2025

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 STATES = [
     "Johor","Kedah","Kelantan","Malacca","Negeri Sembilan","Pahang",
     "Penang","Perak","Perlis","Sabah","Sarawak","Selangor",
-    "Terengganu","Kuala Lumpur","Labuan","Putrajaya"
+    "Terengganu","Kuala Lumpur"
 ]
 
 JOBS = ["Virtual Assistant","Marketing Assistant","Data Entry Specialist","Other"]
